8878ad9db12e87f6a6b68f02caa2a28982de3d91
taylor <67763769+chaiznt@users.noreply.github.com>
2023-07-01 01:58:58 -0500

    added placeholder messages for startup and shutdown
    eeeeeeeeeeeeeeeeeeeeee

--- src/main/java/sl/chai/salmonball/SalmonBallZ.java
+++ src/main/java/sl/chai/salmonball/SalmonBallZ.java
@@ -2,16 +2,19 @@ package sl.chai.salmonball;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SalmonBallZ extends JavaPlugin {
-
+public final class SalmonBallZ extends JavaPlugin
+{
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         // Plugin startup logic
-
+        System.out.println("hiyo chat (placeholder)");
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
         // Plugin shutdown logic
+        System.out.println("biyo o/");
     }
 }
