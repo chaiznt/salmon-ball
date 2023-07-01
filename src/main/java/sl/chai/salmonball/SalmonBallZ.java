195b3c5d6c3e153150351c1f2dd8c127e2b333ab
taylor <67763769+chaiznt@users.noreply.github.com>
2023-07-01 01:42:05 -0500

    Add files via upload
    initial commit

---
+++ src/main/java/sl/chai/salmonball/SalmonBallZ.java
@@ -0,0 +1,17 @@
+package sl.chai.salmonball;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SalmonBallZ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
